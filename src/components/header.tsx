b5f9d5c8da8455a9e537d3e9f7a5a0f9c1a0b073
Victor Vu <vu.vu@thebank.vn>
2023-11-30 23:33:39 +0700

    update active link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Button from "./common/button";
 import { ButtonConnectProvider } from "./button_connect_provider";
+import ActiveLink from "./common/active_link";
 
 
 export const Header = () => {
@@ -13,10 +14,10 @@ export const Header = () => {
         <div className="h-[84px] w-full flex justify-between mb-9 items-start border-b-2 border--[#142321]">
           <Image src={Logo} alt="logo alphacado" height={40} />
           <nav className="flex gap-9 text-[#142321] items-center">
-            <Link href={"/app/swap"} className="text-[18px] hover:font-semibold font-bold">Zap</Link>
-            <Link href={"/app/swap"} className="text-[18px] hover:font-semibold">Deposit</Link>
-            <Link href={"/app/vault/create"} className="text-[18px] hover:font-semibold">Create Vault</Link>
-            <Link href={"/app/swap"} className="text-[18px] hover:font-semibold">Analytics</Link>
+            <ActiveLink href={"/app/swap"} className="text-[18px] hover:font-semibold">Zap</ActiveLink>
+            <ActiveLink href={"/app/deposit"} className="text-[18px] hover:font-semibold">Deposit</ActiveLink>
+            <ActiveLink href={"/app/vault/create"} className="text-[18px] hover:font-semibold">Create Vault</ActiveLink>
+            <ActiveLink href={"/app/analytic"} className="text-[18px] hover:font-semibold">Analytics</ActiveLink>
             <ButtonConnectProvider className="h-12">
 
             </ButtonConnectProvider>
