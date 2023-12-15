e37c3d44ed5e0765b73df3aed864740a985fb214
Victor Vu <vu.vu@thebank.vn>
2023-12-15 15:17:02 +0700

    add no cache

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    config.module.unknownContextCritical = false
-    return config
-  }
-}
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.module.unknownContextCritical = false;
+    return config;
+  },
 
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        source: "/api/vaults",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
