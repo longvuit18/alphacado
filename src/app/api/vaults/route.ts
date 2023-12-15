98d9275cabad9ce2885d219dbc4534afc1b51982
Victor Vu <vu.vu@thebank.vn>
2023-12-15 16:06:50 +0700

    fix cache

--- src/app/api/vaults/route.ts
+++ src/app/api/vaults/route.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
+export const revalidate = 0
 export async function GET() {
   const res = await axios.get("http://34.87.73.123:5005/api/v1/vaults");
   return NextResponse.json(res.data.data)
